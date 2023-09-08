a5ca7dae1bb40849c4ececba0294875f5fb94ded
Anasse LAARABI <contact.laarabi@gmail.com>
2023-09-08 22:03:48 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,49 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Alert, FlatList, Keyboard, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
 import Header from './components/header';
 import ListTodo from './components/listTodo';
 import AddTodo from './components/addTodo';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [todos, setTodos] = useState([
-    // {text: 'buy coffee', key:'1'},
-    // {text: 'Complete the App', key:'2'},
-    // {text: 'Study', key:'3'},
-  ]);
+  const [todos, setTodos] = useState([]);
 
-  const [keyCounter, setKeyCounter] = useState(1)
+  const [keyCounter, setKeyCounter] = useState(1);
 
-  // const [doneTodos, setDoneTodos] = useState([]);
+  const [fontsLoader, setFontsLoader] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          'RubikIso': require('./assets/fonts/RubikIso-Regular.ttf')
+        })
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setFontsLoader(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoader) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoader]);
+
+  if (!fontsLoader) {
+    return null;
+  }
+
 
   const doneHandler = (key) => {
-    // setDoneTodos({})
     Alert.alert('Are u Sure ?', 'this todo will be deleated are you sure?', [
       {text: 'YES', onPress: () => {
         setTodos((prevTodos) => {
@@ -40,35 +67,38 @@ export default function App() {
     }
   }
 
-
-  return (
-    <TouchableWithoutFeedback onPress={() => {
-      Keyboard.dismiss();
-    }}>
-      <View style={styles.container}>
-        <Header />
-        <View style={styles.content}>
-          <AddTodo submitHandler={submitHandler} />
-          <FlatList
-          data={todos}
-          renderItem={({ item }) => (
-            <ListTodo todo={item} doneHandler={doneHandler} />
-          )}
-          />
-        </View>
-      </View>
-    </TouchableWithoutFeedback>
-  );
+    return (
+        <TouchableWithoutFeedback onPress={() => {
+          Keyboard.dismiss();
+        }}>
+          <View style={styles.container} onLayout={onLayoutRootView}>
+            <Header />
+            <View style={styles.content}>
+              <AddTodo submitHandler={submitHandler} />
+              <FlatList
+              style= {styles.listTodos}
+              data={todos}
+              renderItem={({ item }) => (
+                <ListTodo todo={item} doneHandler={doneHandler} />
+              )}
+              />
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F2E8C6',
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
   content: {
+    flex: 1,
     padding: 20,
+  },
+  listTodos: {
+    flex: 1,
   }
 });
