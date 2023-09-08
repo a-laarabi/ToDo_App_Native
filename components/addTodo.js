faf9de92da68ab94406f0c41daded3bab7997b76
Anasse LAARABI <contact.laarabi@gmail.com>
2023-09-08 22:05:10 +0100

    Update addTodo.js

--- components/addTodo.js
+++ components/addTodo.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
-import { TextInput, View, StyleSheet, Button } from "react-native";
+import { TextInput, View, StyleSheet, Button, Text } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+import ButtonAdd from "../shared/ButtonAdd";
 
 const AddTodo = ({submitHandler}) => {
 
@@ -11,25 +13,35 @@ const AddTodo = ({submitHandler}) => {
 
 
   return (
-    <View>
+    <View style={styles.addTodo}>
       <TextInput
+        multiline
         style={styles.todoInput}
         placeholder="new Todo.."
         onChangeText={(val)=>{setText(val)}}
       />
-      <Button onPress={pressHandler} title ='add todo' color='#A73121'/>
+      {/* <Button onPress={pressHandler} title ='add todo' color='#A73121'/> */}
+      <ButtonAdd onPress={pressHandler} >
+        <Ionicons name="add-circle" size={20} />
+        <Text>Add toDo</Text>
+      </ButtonAdd>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  addTodo:{
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+
   todoInput:{
     backgroundColor: '#DAD4B5',
     borderBottomColor: '#000',
     borderStyle:'solid',
     borderBottomWidth: 1,
     padding: 15,
-    marginBottom: 13
+    width: '70%'
   }
 })
 
